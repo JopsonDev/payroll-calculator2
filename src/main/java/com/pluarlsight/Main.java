dc85fd6fa405bef9d919b2614879c36d56c4d82e
JopsonDev <jopsonmason@outlook.com>
2026-04-21 09:26:47 -0400

    made it a bit more readable

--- src/main/java/com/pluarlsight/Main.java
+++ src/main/java/com/pluarlsight/Main.java
@@ -8,15 +8,20 @@ public class Main {
             try {
                 FileReader payroll = new FileReader("list.csv");
                 BufferedReader reader = new BufferedReader(payroll);
+
                 String line;
+
                 while ((line = reader.readLine()) != null) {
                     String[] parts = line.split("\\|");
+
                     int id = Integer.parseInt(parts[0]);
                     String name = parts[1];
                     double hoursWorked = Double.parseDouble(parts[2]);
                     double payRate = Double.parseDouble(parts[3]);
+
                     Employee first = new Employee(id, name, hoursWorked, payRate);
-                    System.out.printf("ID: %d | Name: %s | Grosspay: %.2f%n", id, name, first.getGrossPay());
+
+                    System.out.printf("ID: %d | Name: %s | Gross pay: %.2f%n", id, name, first.getGrossPay());
                 }
             } catch (Exception a) {
                 System.out.println("Oh no something went wrong...");
